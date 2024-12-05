5237e1a713c69231ae98954a4c72a1c981d057e3
daraakitoyemru <dakit711@mtroyal.ca>
2024-12-05 15:59:14 -0700

    added more helpers to populate tables, will clean up code soon

--- app.js
+++ app.js
@@ -8,26 +8,86 @@ import {
 document.addEventListener("DOMContentLoaded", () => {
   const domain = "https://www.randyconnolly.com/funwebdev/3rd/api/f1/";
   const homeSection = document.querySelector("#homeView");
+  const raceViewTitle = document.querySelector("#racesView h2");
+  const keyList = ["raceData", "resultData", "qualifyingData"];
+  const queryList = ["races.php?", "results.php?", "qualifying.php?"];
   // i'll refactor this later. this happens as soon as the DOM loads,
-  // it supposed to happen after a user selects a season
-  Promise.all([
-    writeToCache(domain + "races.php?", "raceData"),
-    writeToCache(domain + "results.php?", "resultData"),
-    writeToCache(domain + "qualifying.php?", "qualifyingData"),
-  ]).then((response) => {
-    console.log(response);
-  });
-  /* Basically if we wanna get specific data, we can use readFromCache() and then use
-find() or filter() to get specific results based on user input.
-*/
 
-  homeSection.addEventListener("click", (e) => {
+  async function checkLocalStorage(key, query) {
+    let localStorageMembers = readFromCache(key);
+    if (!localStorageMembers) {
+      await writeToCache(domain + query, key);
+      localStorageMembers = readFromCache(key);
+    }
+    return localStorageMembers;
+  }
+
+  function createElement(selector, attributes = {}, textContent = null) {
+    const element = document.createElement(selector);
+
+    if (JSON.stringify(attributes) !== "{}") {
+      //credits : https://masteringjs.io/tutorials/fundamentals/foreach-object
+      Object.entries(attributes).forEach((entry) => {
+        const [key, value] = entry;
+        element.setAttribute(key, value);
+      });
+    }
+    if (textContent) {
+      element.textContent = textContent;
+    }
+    return element;
+  }
+
+  function addTableRow(parentElementSelector, object, propArr, element = null) {
+    const parent = document.querySelector(parentElementSelector);
+    const tr = document.createElement("tr");
+
+    propArr.forEach((property) => {
+      if (object.hasOwnProperty(property)) {
+        const td = document.createElement("td");
+        td.textContent = object[property];
+        tr.appendChild(td);
+      }
+    });
+
+    if (element) {
+      tr.appendChild(element);
+    }
+
+    parent.appendChild(tr);
+  }
+
+  homeSection.addEventListener("click", async (e) => {
+    document.querySelector("#racesBody").replaceChildren();
     if (e.target.nodeName === "OPTION") {
-      let value = e.target.value;
-      console.log(`${e.target} clicked`);
+      /* 1. clear home view section
+        2. Check local storage, populate if null
+        3. Show loading animation
+        4. Populate table, hide loading animation
+      */
+      let year = e.target.value;
+      const localStorageMembers = await Promise.all([
+        checkLocalStorage(`raceData${year}`, "races.php?season=" + year),
+        checkLocalStorage(`resultData${year}`, "results.php?season=" + year),
+
+        checkLocalStorage(
+          `qualifyingData${year}`,
+          "qualifying.php?season=" + year
+        ),
+      ]);
+
+      const [raceData, resultData, qualifyingData] = localStorageMembers;
+
+      raceViewTitle.textContent = `Races for ${year}`;
+      raceData.forEach((raceObj) => {
+        let link = createElement(
+          "a",
+          { href: "#", "data-id": raceObj["id"] },
+          "View Race"
+        );
+        addTableRow("#racesBody", raceObj, ["round", "name"], link);
+      });
       modifyStyle("#racesView", "display", "block");
-      //writeToCache(domain + `races.php?${value}`, "data");
-      console.log(readFromCache("raceData"));
     }
   });
 });
